2f4e3ee1158b480c1b8bc20e5fa716dbeb54bf52
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2021-11-04 13:02:05 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/NavBar/DataTable";
+import Footer from "components/NavBar/footer";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
